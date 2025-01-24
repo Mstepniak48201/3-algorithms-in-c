07062a38df5189c3671a41e780fa4cf89fcd6df8
Michael Stepniak <stepniak.michael@gmail.com>
2025-01-24 17:50:37 -0500

    working merge() function

--- recursive/recursive.c
+++ recursive/recursive.c
@@ -6,12 +6,12 @@ void merge(int arr[], int l, int r, int m);
 
 int main(void)
 {
-  int test_array[] = {3, 1};
+  int test_array[] = {1, 5, 100, 6, 90};
   int length = sizeof(test_array) / sizeof(test_array[0]);
   
   print_array(test_array, length);
 
-  merge(test_array, 0, 1, 0);
+  merge(test_array, 0, 4, 2);
 }
 
 void merge(int arr[], int l, int r, int m)
@@ -28,6 +28,14 @@ void merge(int arr[], int l, int r, int m)
   // Declare temp arrays.
   int left[l_length], right[r_length];
 
+  // Declare index variables to track status of merge
+  // for the original array, left and right temp arrays.
+  // Altering the original array will always occur at the 
+  // l value.
+  int arr_i = l;
+  int left_i = 0;
+  int right_i = 0;
+
   // Copy left half of arr[] into left[].
   for (int i = 0; i < l_length; i++)
   {
@@ -39,12 +47,59 @@ void merge(int arr[], int l, int r, int m)
   {
     right[i - lr_index] = arr[i];
   }
+
+  // Now that the arrays are copied, compare them.
+  // Keep track of the index set for the original array,
+  // and the indices of the left and right arrays.
+  // If the left or right index exceeds its length, exit
+  // the loop.
+  while (left_i < l_length && right_i < r_length)
+  {
+    if (left[left_i] <= right[right_i])
+    {
+      arr[arr_i] = left[left_i];
+      left_i++;
+    }
+    else
+    {
+      arr[arr_i] = right[right_i];
+      right_i++;
+    }
+    arr_i++;
+  }
+
+  // Copy remaining elements from left array.
+  while (left_i < l_length)
+  {
+    arr[arr_i] = left[left_i];
+    arr_i++;
+    left_i++;
+  }
+
+  // Copy remaining elements from right array.
+  while (right_i < r_length)
+  {
+    arr[arr_i] = right[right_i];
+    arr_i++;
+    right_i++;
+  }
+
+  printf("First char of array: \n");
+  printf("%i\n", arr[0]);
     
+  printf("Second char of array: \n");
+  printf("%i\n", arr[1]);
+
+  printf("Sorted Array: \n");
+  print_array(arr, length);
+
   printf("left: ");
   print_array(left, l_length);
 
   printf("right: ");
   print_array(right, r_length);
+
+  
 
 
 
